b130c6af24773175a480bd19e773eeaa2b5f6b5a
Geraldo Júnior <junior.bimbo@gmail.com>
2021-03-03 22:06:40 -0300

    feat: add controls buttons

--- src/components/Clock/Timer.js
+++ src/components/Clock/Timer.js
@@ -4,20 +4,26 @@ import ReactCountdownClock from 'react-countdown-clock';
 import styled from 'styled-components'
 import { Colors } from '../../utils/Colors'
 
-export default function Timer({durationInSeconds, paused=false}) {
+export default function Timer({durationInSeconds, paused, endTime}) {
   return (
     <Container>
       <ReactCountdownClock
         seconds={durationInSeconds}
-        color={Colors.primary}
+        color={Colors.secondaryLight}
         alpha={0.9}
-        size={300}
-        paused
+        size={200}
+        paused={paused}
+        onComplete={ () => endTime(true) }
       />
     </Container>
   )
 }
 
 const Container = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  padding: 1rem 0;
 `
--- src/components/Clock/index.tsx
+++ src/components/Clock/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Timer from './Timer'
 
@@ -7,14 +7,34 @@ import { Colors, IColors } from '../../utils/Colors'
 
 export default function Clock() {
   const [playing, setPlaying] = useState<boolean>(false)
+  const [endTime, setEndTime] = useState<boolean>(false)
+
+  const handlePlayer = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    e.preventDefault()
+
+    setPlaying(!playing)
+  }
+
+  const handleEndTimer = (timeIsLeft: boolean): void => {
+    setEndTime(timeIsLeft)
+  }
 
   return (
-    <Container bgColor={'dark'} textColor={'light'}>
-      <div>Tempo Restante</div>
+    <Container bgColor={'lighter'} textColor={'light'}>
+      { !endTime ?
+      <>
+        <Title textColor={'secondaryLight'}>Tempo Restante</Title>
 
-      <Timer durationInSeconds={10} paused={false} />
+        <Timer durationInSeconds={10} paused={!playing} endTime={handleEndTimer} />
 
-      <div>{playing ? 'Pausar' : 'Continuar'}</div>
+        <Controls>
+          <PlayerButton playing={playing} onClick={e => handlePlayer(e)}>
+            {playing ? 'Pausar' : 'Continuar'}
+          </PlayerButton>
+        </Controls> 
+      </>:
+        <MessageTimeEnd>Tempo Encerrado!</MessageTimeEnd>
+      }
     </Container>
   )
 }
@@ -26,4 +46,52 @@ const Container = styled.div<{ bgColor: IColors, textColor: IColors }>`
   align-items: center;
   background-color: ${props => Colors[props.bgColor]};
   color: ${props => Colors[props.textColor]};
+  padding: 1rem 0;
+`
+
+const Title = styled.div<{ textColor: IColors }>`
+  Color: ${props => Colors[props.textColor]};
+`
+
+const Controls = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
+
+const PlayerButton = styled.a<{ playing: boolean }>`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.3rem;
+  padding: 0.4rem 0.8rem;
+  border: ${props => `1px solid ${Colors.light}`};
+  color: ${props => Colors.light};
+  transition: 0.3s;
+  
+  &:hover {
+    cursor: pointer;
+    border-color: ${props => props.playing ? Colors.secondaryLight : Colors.primaryLight};
+    color: ${props => props.playing ? Colors.secondaryLight : Colors.primaryLight};
+  }
+
+`
+
+const MessageTimeEnd = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: ${props => Colors.secondary};
+  font-weight: 700;
+  animation: blinker 0.5s linear infinite;
+  font-size: 2rem;
+  padding: 2rem 0;
+
+  @keyframes blinker {
+    50% { opacity: 0; }
+  }
 `
